8184c9fddfa7bdb8679cdbcbd53d10a601c29a7e
cfrancobedoya <cfrancobedoya@gmail.com>
2016-12-21 18:30:07 -0500

    NO TENER ENCUENTA

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-const config = require('./config')
-
-
-app.listen(config.port , function (){
-    console.log(`Aplicación corriendo en htpp://localhost:${config.port}`)
-    console.log("Desarrollado por: Cristian Franco Bedoya, Santiago R. Alvarez Gomez");
-    console.log("Franco Software 2016");
-});
