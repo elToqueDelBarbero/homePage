49bdca0f0a39658b03601bec38da466f7d667044
cfrancobedoya <cfrancobedoya@gmail.com>
2017-01-17 15:52:49 -0500

    FIREBASE CONECTION
    Se agrega un script en un archivo firebaseConection.js, y se agrega al
    index.html.

---
+++ public/js/firebaseConection.js
@@ -0,0 +1,13 @@
+
+// Initialize Firebase
+  /**
+  *** CÓDIGO GENERADO POR FIREBASE DESDE LA WEB
+  **/
+var config = {
+  apiKey: "AIzaSyDwltN0EXmTfEjSZyIv-zRYvrs1x9nMOVM",
+  authDomain: "eltoquedelbarbero-4741f.firebaseapp.com",
+  databaseURL: "https://eltoquedelbarbero-4741f.firebaseio.com",
+  storageBucket: "eltoquedelbarbero-4741f.appspot.com",
+  messagingSenderId: "625920625709"
+};
+firebase.initializeApp(config);
